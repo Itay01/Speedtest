352455cc2e61cb90d5e7aa39d0a08e27996f1b2a
itaym <itaymarom07@gmail.com>
2024-01-26 16:24:08 +0200

    Move sleep action to the top of the loop.

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def convert_to_mbps(value):
 st = Speedtest()
 df = pd.DataFrame(columns=['Time', 'Download'])
 for i in tqdm(range(5)):
+    if i != 0:
+        time.sleep(120)
     download = convert_to_mbps(st.download())
     df = df._append({'Time': datetime.now().strftime("%H-%M-%S"), 'Download': download}, ignore_index=True)
-    time.sleep(120)
 
 df.to_csv('speedtest.csv', index=False)
